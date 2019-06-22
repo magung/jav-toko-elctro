8511ae53e531922415f1d33488180a23d434d7cd
magung <50833200+magung@users.noreply.github.com>
2019-06-22 16:54:34 +0700

    Add files via upload

---
+++ toko_agung_elektro/src/toko_agung_elektro/Toko_agung_elektro.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package toko_agung_elektro;
+
+/**
+ *
+ * @author PLJC-PC
+ */
+public class Toko_agung_elektro {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        MenuUtama m = new MenuUtama();
+        m.setExtendedState(MenuUtama.MAXIMIZED_BOTH);
+        m.setVisible(true);
+    }
+    
+}
